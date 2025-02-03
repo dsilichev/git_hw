7ea2082ebb6baf90050b03449ad13e87b2321bc3
Dmitriy <dsilichev@gmail.com>
2025-02-03 11:55:15 +0300

    some changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 import cowsay from 'cowsay';
 //const cowsay = require('cowsay');
-console.log(cowsay.say({ text: 'Hello Another World' }));
-console.log(cowsay.say({ text: 'Hello Sweet Cow' }));
+console.log(cowsay.say({ text: 'Hello World' }));
+console.log(cowsay.say({ text: 'Hello Cow' }));
